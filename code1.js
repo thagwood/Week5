b656fb906c016fdf56eef9415b5f0c740a713a2b
thagwood <38319072+thagwood@users.noreply.github.com>
2022-02-14 00:24:33 -0600

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,81 @@
+function start() {
+    document.getElementById("data").rows["seconds"].innerHTML = "Reading data...";
+    //I'm assuming this is where my code goes wrong and im un able to figure out what wrong
+    index = 0;
+    timer = setInterval(updateDispay(), time_interval);
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+
+}
+
+function stop() {
+    clearInterval(timer);
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+}
+
+//making a class to hold data
+class InputData {
+    constructor(
+        time_seconds,
+        latitude,
+        longitude,
+        gps_altitude,
+        bmpSensor_altitude,
+        bmpSensor_pressure,
+        bmpSensor_temp,
+        digSensor_temp,
+        cssSensor_temp,
+        cssSensor_eCO2,
+        cssSensor_TVOC,
+        UV,
+        accelX,
+        accelY,
+        accelZ,
+        magneticX,
+        magneticY,
+        magneticZ,
+        gyroX,
+        gyroY,
+        gyroZ
+    ) {
+        this.time_seconds = time_seconds;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.gps_altitude = gps_altitude;
+        this.bmpSensor_altitude = bmpSensor_altitude;
+        this.bmpSensor_pressure = bmpSensor_pressure;
+        this.bmpSensor_temp = bmpSensor_temp;
+        this.digSensor_temp = digSensor_temp;
+        this.cssSensor_temp = cssSensor_temp;
+        this.cssSensor_eCO2 = cssSensor_eCO2;
+        this.cssSensor_TVOC = cssSensor_TVOC;
+        this.UV = UV;
+        this.accelX = accelX;
+        this.accelY = accelY;
+        this.accelZ = accelZ;
+        this.magneticX = magneticX;
+        this.magneticY = magneticY;
+        this.magneticZ = magneticZ;
+        this.gyroX = gyroX;
+        this.gyroY = gyroY;
+        this.gyroZ = gyroZ;
+    }
+}
+
+//function to getData
+function getData() {
+    var loadedData = loadData();
+    return loadedData;
+}
+
+//function to format table display
+function dataRow(legend, value, units){
+    msg = "<td>";
+    msg += legend;
+    msg += ": </td><td>";
+    msg += value;
+    msg += " " + units;
+    msg += "</td>";
+    return msg;
+}
